7d14b45907d4e183d988c299ca7f467654e92875
GabiestDev <gabrielnetolobo@gmail.com>
2025-08-19 08:20:57 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using QRCoder; 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string textoParaQRCode = "https://wa.me/5511987654321";
+
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(textoParaQRCode, QRCodeGenerator.ECCLevel.Q);
+
+        PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+        byte[] qrCodeAsPngByteArr = qrCode.GetGraphic(20);
+
+        string caminhoArquivo = "meu_qrcode.png";
+        File.WriteAllBytes(caminhoArquivo, qrCodeAsPngByteArr);
+
+        Console.WriteLine($"QR Code salvo em: {Path.GetFullPath(caminhoArquivo)}");
+    }
+}
